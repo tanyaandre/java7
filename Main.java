5489edccb9a3155a9bd63187465836a2b04101ab
tanyaandre <tanich192837@gmail.com>
2021-03-24 22:47:09 +0400

    Added homework

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        long price = 5450;
+        long bonus_in_miles = price / 20;
+        System.out.println(bonus_in_miles);
+    }
+}
